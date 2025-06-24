df7c682407f2da193915abf96734c68266a85257
PeterWolfframm <servus@peterwolfframm.com>
2025-06-24 20:02:29 +0200

    energy producer initial

---
+++ energy-producer/src/test/java/at/uastw/energy/energyproducer/EnergyProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package at.uastw.energy.energyproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
